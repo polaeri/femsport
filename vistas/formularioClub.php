b896424879d5b8c02cc88b21e59c9c92a691c4f4
ipicazo <ipicazomartinez@gmail.com>
2015-05-13 21:14:09 +0200

    añadido la intro de xavi

--- vistas/formularioClub.php
+++ vistas/formularioClub.php
@@ -1,8 +1,45 @@
-<?php
+<html>
+    <head>
+        <title>FemSport</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
+    <body>
+        <header><img src="logoFemEsport2.png">
+            <menu>
+                <ul>
+                    <li><a href="#">Inicio</a></li>
+                    <li><a href="#">Perfil</a></li>
+                    
+                </ul>
+            </menu>
+        </header>
+        
+        <article>CONTENIDO  asdasd   asdasd
+        
+            
+                    <h1>Formulario Club</h1>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+                    <form action="index.php" method="post">
+                        
+                        
+                        <input type="text" name="nombre" value="" placeholder="Nombre" required/>
+                        <input type="text" name="cif" value="" placeholder="CIF" required/>
+                        <input type="text" name="telefono" value="" />
+                        <input type="text" name="nombre" value="" />
+                        <input type="text" name="nombre" value="" />
+                        <input type="text" name="nombre" value="" />
+                        
+                        <input type="submit" value="Registrar" />
+                    </form>
+                
+            
+           
+        
+        <sidebar>WIDGET FACEBOOK</sidebar>
+        </article>
+        
+        <footer>Copyright ©FemSport</footer>
+    </body>
+</html>
