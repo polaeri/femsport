dbca0fbc2d984f1361022ea9e302d488bae69e20
polaeri <polarandagirona@gmail.com>
2015-05-20 19:09:23 +0200

    modificado controlador para que se modifque el perfil del jugador modificada conexion para modificar datos base de datos añadido funcion jugador modificar datos añadido contenido perfil jugador y conexio

--- modelo/Conexio.php
+++ modelo/Conexio.php
@@ -5,8 +5,7 @@ class Conexio {
     private $connexio = null;
 
     function __construct() {
-//$this->connexio = new mysqli("endurorocks.com", "femsport", "xpid2015", "femsport");
-        $this->connexio = new mysqli("localhost", "polaeri", "123456", "femsport");
+        $this->connexio = new mysqli("endurorocks.com", "femsport", "xpid2015", "femsport");
     }
 
     function tancarConexio() {
